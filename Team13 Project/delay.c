2468a7ddf070aba6f9cd43d81144d612390b5a69
ducqh8a <108819964+ducqh8a@users.noreply.github.com>
2023-05-24 20:46:52 +0700

    Update delay.c

--- Team13 Project/delay.c
+++ Team13 Project/delay.c
@@ -4,8 +4,8 @@ volatile uint32_t usTicks;
 /* Init Systick to 1 TICK = 1 microsecond*/
 void SysTick_Init(void) {
 	
-  	SysTick->LOAD = SystemCoreClock / 1000000; //Configured the SysTick to count in 1us
-	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk |SysTick_CTRL_TICKINT_Msk |SysTick_CTRL_ENABLE_Msk; //Select Core Clock & Enable SysTick & Enable Interrupt
+  	SysTick->LOAD = SystemCoreClock / 1000000; 							//Configured the SysTick to count in 1us
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk |SysTick_CTRL_TICKINT_Msk |SysTick_CTRL_ENABLE_Msk; 	//Select Core Clock & Enable SysTick & Enable Interrupt
 }
 /* Function Delay microsecond*/
 void delay (uint32_t TICK) { 
