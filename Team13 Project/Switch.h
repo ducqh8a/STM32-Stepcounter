9fdf66df222bbc854e97955424c0c18dcc37d30f
ducqh8a <108819964+ducqh8a@users.noreply.github.com>
2023-05-24 18:12:57 +0700

    Update Switch.h

--- Team13 Project/Switch.h
+++ Team13 Project/Switch.h
@@ -3,8 +3,8 @@
 
 #include "stm32f10x.h"
 
-#define Pin_SS               		 ((uint16_t)0x1000)  /*!< Pin 12 selected */
-#define Pin_RESET                ((uint16_t)0x2000)  /*!< Pin 13 selected */
+#define Pin_SS                    ((uint16_t)0x1000)  /*!< Pin 12 selected */
+#define Pin_RESET                 ((uint16_t)0x2000)  /*!< Pin 13 selected */
 
 void switch_init(void);
 uint8_t input(uint16_t GPIO_Pin);
